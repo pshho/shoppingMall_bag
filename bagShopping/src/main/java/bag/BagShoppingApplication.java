6d6eb5122fb0f3e09ee30eb816d028f4f138d509
pshho <awdr498@gmail.com>
2023-09-01 11:17:40 +0900

    project start

---
+++ bagShopping/src/main/java/bag/BagShoppingApplication.java
@@ -0,0 +1,13 @@
+package bag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagShoppingApplication.class, args);
+	}
+
+}
---
+++ bagShopping/src/test/java/bag/BagShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package bag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
