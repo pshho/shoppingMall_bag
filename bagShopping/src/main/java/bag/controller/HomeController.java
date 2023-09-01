aba45e80c196c5f6f3d7ee126cb8751d0c09ccbf
pshho <awdr498@gmail.com>
2023-09-01 12:33:05 +0900

    indexPage

---
+++ bagShopping/src/main/java/bag/controller/HomeController.java
@@ -0,0 +1,14 @@
+package bag.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@GetMapping
+	String goHome() {
+		return "index";
+	}
+}
