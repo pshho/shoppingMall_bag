59ae28ad62ee2ce7b499db02e0e64ed80a5b91cf
pshho <awdr498@gmail.com>
2023-09-08 21:08:29 +0900

    payments ing

--- bagShopping/src/main/java/bag/controller/MemberController.java
+++ bagShopping/src/main/java/bag/controller/MemberController.java
@@ -149,7 +149,7 @@ public class MemberController {
 				msg = "로그인 되었습니다.";
 				goUrl = "/";
 				session.setAttribute("userId", mdto.getMemberId());
-				session.setMaxInactiveInterval(10);
+				session.setMaxInactiveInterval(1800);
 			}
 		}
 		
--- bagShopping/src/main/java/bag/service/SessionListener.java
+++ bagShopping/src/main/java/bag/service/SessionListener.java
@@ -3,30 +3,18 @@ package bag.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import jakarta.servlet.annotation.WebListener;
 import jakarta.servlet.http.HttpSessionEvent;
 import jakarta.servlet.http.HttpSessionListener;
 
-@WebListener
 @Service
 public class SessionListener implements HttpSessionListener {
 	
 	@Autowired
 	CartMapper cartMapper;
 	
-	String userId;
-
-	@Override
-	public void sessionCreated(HttpSessionEvent se) {
-		userId = (String)se.getSession().getAttribute("userId");
-	}
-	
 	@Override
 	public void sessionDestroyed(HttpSessionEvent se) {
-		String userId = (String)se.getSession().getAttribute("userId");
-		if(se.getSession() == null) {
-			System.out.println("작동?");
-			cartMapper.allDelete(userId);
-		}
+		String user = (String)se.getSession().getAttribute("userId");
+		cartMapper.allDelete(user);
 	}
 }
